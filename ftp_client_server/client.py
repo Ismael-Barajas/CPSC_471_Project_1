88483d7501bc12e2041d6c52cff943cd53aff433
Ismael-barajas <ismaelbarajas.dev@gmail.com>
2021-11-07 19:17:08 -0800

    Commented code and made minor adjustments

--- ftp_client_server/client.py
+++ ftp_client_server/client.py
@@ -4,7 +4,6 @@ import sys
 import os
 
 FORMAT = "utf-8"
-MESSAGE_SIZE = 1024
 PORT_SIZE = 10
 RESPONSE_SIZE = 4096
 
@@ -13,30 +12,36 @@ def main():
     # Pull localhost IP or url and the port number from the command line
     cIP = sys.argv[1]
     cPortNumber = int(sys.argv[2])
+
     # Create socket connection with server.py
     cSocket = socket(AF_INET, SOCK_STREAM)
     cSocket.connect((cIP, cPortNumber))
-    print(f"connected to {cIP}:{cPortNumber}")
+
+    print(f"Connected to server at: {cIP}:{cPortNumber}")
 
     while 1:
         # Splits user input by white space into a list
         command = input("ftp> ").split()
 
-        if command[0] == "exit":
-            # Check to see if any other arguments are passed with exit
+        if command[0] == "quit":
+            # Check to see if any other arguments are passed with quit
             if len(command) != 1:
                 print(
-                    f"Please make sure there are no other arguments when using exit command\n"
+                    f"Please make sure there are no other arguments when using 'quit' command\n"
                 )
             else:
+                # Join list back into a string and send encoded string to server
                 commandString = " ".join(command)
                 cSocket.send(commandString.encode(FORMAT))
 
+                # Close main connection with server
                 cSocket.close()
 
                 print(
                     f"Disconnecting from the server and shutting down the client. Bye!"
                 )
+
+                # Terminate script
                 sys.exit(0)
 
         elif command[0] == "ls":
@@ -46,10 +51,11 @@ def main():
                     "Please make sure there are no other arguments when using ls command\n"
                 )
             else:
-                # " ".join(command)
+                # Join list back into a string and send encoded string to server
                 commandString = " ".join(command)
                 cSocket.send(commandString.encode(FORMAT))
 
+                # Method used to ensure every byte is received from the server
                 data = b""
                 while True:
                     part = cSocket.recv(PORT_SIZE)
@@ -57,20 +63,32 @@ def main():
                     if len(part) < PORT_SIZE:
                         break
 
+                # Decode the port number of the ephemeral socket created in the server
                 ephemeralSocketPortNumberDecoded = int(data.decode(FORMAT))
 
-                print(f"Ephemeral port {ephemeralSocketPortNumberDecoded}")
-
+                # Establish connection with ephemeral socket to begin receiving info from the server
                 ephemeralSocket = socket(AF_INET, SOCK_STREAM)
                 ephemeralSocket.connect((cIP, ephemeralSocketPortNumberDecoded))
 
+                print(
+                    f"Connection established with ephemeral socket at {cIP}:{ephemeralSocketPortNumberDecoded}\n"
+                )
+
+                # Method used to ensure every byte is received from the ephemeral socket created in the server
                 data = b""
                 while True:
                     part = ephemeralSocket.recv(RESPONSE_SIZE)
                     data += part
                     if len(part) < RESPONSE_SIZE:
                         break
+
+                # Decode received data from the ephemeral socket and display to client
                 dataDecoded = data.decode(FORMAT)
+
+                # Close connection with the ephemeral socket
+                ephemeralSocket.close()
+
+                # Display "ls" respose from server to client
                 print(f"{dataDecoded}")
 
         elif command[0] == "get":
@@ -80,11 +98,14 @@ def main():
                     "Please make sure there are no more than 1 argument when using get command\n"
                 )
             else:
+                # Pull file name from cli
                 fName = command[1]
 
+                # Join list back into a string and send encoded string to server
                 commandString = " ".join(command)
                 cSocket.send(commandString.encode(FORMAT))
 
+                # Method used to ensure every byte is received from the ephemeral socket created in the server
                 data = b""
                 while True:
                     part = cSocket.recv(PORT_SIZE)
@@ -92,30 +113,44 @@ def main():
                     if len(part) < PORT_SIZE:
                         break
 
+                # Decode the port number of the ephemeral socket created in the server
                 ephemeralSocketPortNumberDecoded = int(data.decode(FORMAT))
 
-                print(f"Ephemeral port {ephemeralSocketPortNumberDecoded}")
-
+                # Establish connection with ephemeral socket to begin receiving info from the server
                 ephemeralSocket = socket(AF_INET, SOCK_STREAM)
                 ephemeralSocket.connect((cIP, ephemeralSocketPortNumberDecoded))
 
+                print(
+                    f"Connection established with ephemeral socket at {cIP}:{ephemeralSocketPortNumberDecoded}"
+                )
+
+                # Method used to ensure every byte is received from the ephemeral socket created in the server then written
                 data = b""
+                # Open fName locally, "wb" open file for writing in binary mode
                 with open(fName, "wb") as f:
                     while True:
                         part = ephemeralSocket.recv(RESPONSE_SIZE)
                         data += part
+                        # If there is no respose from the server then the file does not exist.
                         if data == b"":
                             print(f"Server has no file with the name of '{fName}'")
                             break
+                        # Once all data is receive execute this block of code
                         if len(part) < RESPONSE_SIZE:
+                            # Write data to file
                             f.write(data)
                             print(
                                 f"{len(data)} bytes have been successfully received from the server."
                             )
                             break
+                    # Close the file once the data has been written
                     f.close()
+
+                # If the file does not exist on the server, delete created file locally that was created in anticipation of the file data
                 if data == b"":
                     os.remove(fName)
+
+                # Close connection with the ephemeral socket
                 ephemeralSocket.close()
 
         elif command[0] == "put":
@@ -125,16 +160,19 @@ def main():
                     "Please make sure there are no more than 1 argument when using put command\n"
                 )
             else:
-                # pull file name from cli
+                # Pull file name from cli
                 fName = command[1]
 
+                # Check to see if provided file name exist in current directory
                 if not os.path.exists(fName):
                     print(f"{fName} does not exist or not found in current directory.")
                     continue
 
+                # Join list back into a string and send encoded string to server
                 commandString = " ".join(command)
                 cSocket.send(commandString.encode(FORMAT))
 
+                # Method used to ensure every byte is received from the ephemeral socket created in the server
                 data = b""
                 while True:
                     part = cSocket.recv(PORT_SIZE)
@@ -142,25 +180,34 @@ def main():
                     if len(part) < PORT_SIZE:
                         break
 
+                # Decode the port number of the ephemeral socket created in the server
                 ephemeralSocketPortNumberDecoded = int(data.decode(FORMAT))
 
-                print(f"Ephemeral port {ephemeralSocketPortNumberDecoded}")
-
+                # Establish connection with ephemeral socket to begin receiving info from the server
                 ephemeralSocket = socket(AF_INET, SOCK_STREAM)
                 ephemeralSocket.connect((cIP, ephemeralSocketPortNumberDecoded))
 
+                print(
+                    f"Connection established with ephemeral socket at {cIP}:{ephemeralSocketPortNumberDecoded}"
+                )
+
+                # Open fName locally, "rb" open file for reading in binary mode
                 with open(fName, "rb") as f:
+                    # Send all the file's data to ephemeral socket on the server
                     ephemeralSocket.sendall(f.read())
 
+                # Close connection with the ephemeral socket
                 ephemeralSocket.close()
 
+                # Display number of bytes transferred to server
                 print(
                     f"{os.path.getsize(fName)} bytes have been successfully transferred to the server."
                 )
 
+        # If an invalid command passed then execute this
         else:
             print(
-                f"Invalid command. Commands available: ls, get <filename>, put <filename>, exit\n"
+                f"Invalid command. Commands available: ls, get <filename>, put <filename>, quit\n"
             )
 
 
--- ftp_client_server/server.py
+++ ftp_client_server/server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from socket import *
-from subprocess import PIPE
 import subprocess
 import sys
 import threading
@@ -10,115 +9,165 @@ FORMAT = "utf-8"
 MESSAGE_SIZE = 1024
 RESPONSE_SIZE = 4096
 
-
+# Helper Class to manage the ephemeral socket
 class ephemeralSocket:
+    # Initialize passed in vairables to be accessed through out the class instance
     def __init__(self, cSocket, content=None, timeout=60):
         self.cSocket = cSocket
         self.timeout = timeout
         self.content = content
-        self._listen()
+        self.create_listen()
 
-    def _listen(self):
-        self.socket = socket(AF_INET, SOCK_STREAM)
-        self.socket.settimeout(self.timeout)
-        self.socket.bind(("", 0))
-        self.socket.listen(1)
+    #  Function that creates ephemeralSocket and listen for connection
+    def create_listen(self):
+        self.ephemeralSocket = socket(AF_INET, SOCK_STREAM)
 
-    def getPortNumber(self):
-        return self.socket.getsockname()[1]
+        # Set timeout in seconds, if timeout time is exceeded then and exception is thrown, if no timeout time is provided the default is 60s
+        self.ephemeralSocket.settimeout(self.timeout)
+        self.ephemeralSocket.bind(("", 0))
+        self.ephemeralSocket.listen(1)
 
-    def waitForClient(self):
-        print(f"Waiting for client {self.getPortNumber()}")
-        portNumber = self.getPortNumber()
+    # Function that waits for client to connect to the ephemeral socket instance
+    def wait(self):
+        print(f"Waiting for client on port: {self.ephemeralSocket.getsockname()[1]}")
+
+        # Pull port number from the ephemeral socket instance and send it to client so they can connect to it
+        portNumber = self.ephemeralSocket.getsockname()[1]
         self.cSocket.send(str(portNumber).encode(FORMAT))
-        (cSocket, address) = self.socket.accept()
+
+        # Wait for client to connect to ephemeral socket instance
+        (cSocket, address) = self.ephemeralSocket.accept()
+
+        # Once a connection is made create a client socket variable available to freely send and receive messages/data
         self.cSocket = cSocket
 
 
 def operations(data, cSocket):
+    # Decode and split operation message received from the client
     to_string = data.decode(FORMAT)
     operation = to_string.split()
 
-    if operation[0] == "exit":
+    if operation[0] == "quit":
+        # Client has disconnected from server
         print(f"Client had disconnected")
 
     elif operation[0] == "ls":
+        # Create an ephemeral socket instance providing the client socket and timeout time of 3 seconds
         ephemeralConnection = ephemeralSocket(cSocket, timeout=3)
+
         try:
-            ephemeralConnection.waitForClient()
+            # Wait for TIMEOUT seconds for client to connect to ephemeral socket
+            ephemeralConnection.wait()
         except:
+            # If timeout exception is thrown, return and abort operation
             print(f"Connection failed")
             return
+
+        # Send the ls command results to client
         ephemeralConnection.cSocket.send(
             str.encode(
-                f"{subprocess.run(['ls', './server_files'], stdout=PIPE, stderr=PIPE, universal_newlines=True).stdout}"
+                f"{subprocess.run(['ls', './server_files'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout}"
             )
         )
-        print(f"ls command completed")
+        print(f"'ls' command executed.")
 
     elif operation[0] == "get":
+        # Pull file name client provided message
         fName = operation[1]
+
+        # Create an ephemeral socket instance providing the client socket and timeout time of 3 seconds
         ephemeralConnection = ephemeralSocket(cSocket, timeout=3)
+
         try:
-            ephemeralConnection.waitForClient()
+            # Wait for TIMEOUT seconds for client to connect to ephemeral socket
+            ephemeralConnection.wait()
         except:
+            # If timeout exception is thrown, return and abort operation
             print(f"Connection failed")
             return
 
+        # Check to see if provided file name exist in "server_files" directory
         if not os.path.exists(os.path.join("server_files", fName)):
             print(
                 f"File '{fName}' client is looking to download does not exist in the 'server_files' folder."
             )
+            return
 
+        # Open fName in "server_files" directory, "rb" open file for reading in binary mode
         with open(os.path.join("server_files", fName), "rb") as f:
+            # Send all file data to client
             ephemeralConnection.cSocket.sendall(f.read())
 
+        # Display file and file size transferred to client
         print(
             f"{fName} has been transferred to the client. SIZE: {os.path.getsize(os.path.join('server_files', fName))} bytes transferred."
         )
+        print(f"'get' command executed.")
 
     elif operation[0] == "put":
+        # Pull file name client provided message
         fName = operation[1]
+
+        # Create an ephemeral socket instance providing the client socket and timeout time of 3 seconds
         ephemeralConnection = ephemeralSocket(cSocket, timeout=3)
+
         try:
-            ephemeralConnection.waitForClient()
+            # Wait for TIMEOUT seconds for client to connect to ephemeral socket
+            ephemeralConnection.wait()
         except:
-            print(f"Connection failed")
+            # If timeout exception is thrown, return and abort operation
+            print(f"Connection to ephemeral socket failed.")
             return
 
-        print("put made it past try block")
-
+        # Method used to ensure every byte is received from client
         tempData = b""
         fSize = None
+        # Open/create a file named fName in the "server_files" directory, "wb" open file for writing in binary mode
         with open(os.path.join("server_files", fName), "wb") as f:
             while True:
                 part = ephemeralConnection.cSocket.recv(RESPONSE_SIZE)
                 tempData += part
+                # Once all data is receive execute this block of code
                 if len(part) < RESPONSE_SIZE:
+                    # Save file to fSize to display file size outside of this loop
                     fSize = tempData
+                    # Write data to file
                     f.write(tempData)
                     break
+            # Close the file once the data has been written
             f.close()
 
+        # Display file and file size transferred from client to server
         print(
             f"{fName} has been transferred to server files. SIZE: {len(fSize)} bytes transferred."
         )
+        print(f"'put' command executed.")
 
+    # If operation/command is not available, execute this
     else:
-        print("Operation data not available")
+        print(f"{operation[0]} not available")
 
 
-def cHelper(cSocket, address):
+# Helper function to receive messages from client
+def commandOperationsHelper(cSocket, address):
     print(f"Connected from {address}")
 
+    # Method used to ensure every byte is received from the client
     data = b""
+
+    # Forever listen for client messages to run commands until connection is terminated
     while True:
         part = cSocket.recv(MESSAGE_SIZE)
         data += part
         if len(part) < MESSAGE_SIZE:
+            # Create a thread to execute client's command
             threading.Thread(target=operations, args=(data, cSocket)).start()
-            if data.decode(FORMAT) == "exit":
+
+            # If client wants to terminate connection with server, break this loop to finally terminate the client thread created in main() to prevent looping errors
+            if data.decode(FORMAT) == "quit":
                 break
+
+            # Reset message buffer
             data = b""
 
 
@@ -127,16 +176,27 @@ def main():
     if len(sys.argv) <= 1:
         print(f"Usage: {sys.argv[0]} <port number>")
         return
-    # Pull port info and create connection
+
+    # Pull port number provided
     sPortNumber = int(sys.argv[1])
+
+    # Create socket and bind server port number to server socket
     sSocket = socket(AF_INET, SOCK_STREAM)
     sSocket.bind(("", sPortNumber))
+
+    # Socket now listens for a connection from client with a backlog length of 5 (length of the queue of pending connections allow to this socket)
     sSocket.listen(5)
+
     print(f"Starting server on port {sPortNumber}")
 
     while 1:
+        # Pull info from connected client
         (cSocket, address) = sSocket.accept()
-        threading.Thread(target=cHelper, args=(cSocket, address)).start()
+
+        # Create a thread for client(s) connected to start receiving commands
+        threading.Thread(
+            target=commandOperationsHelper, args=(cSocket, address)
+        ).start()
 
 
 if __name__ == "__main__":
